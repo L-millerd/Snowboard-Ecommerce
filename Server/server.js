ee9798944b646dee62634bdb4cea7bd8e161982c
L-millerd <lisa.millerd@gmail.com>
2022-04-29 17:17:06 -0700

    ecommerce-snowboard

--- Server/server.js
+++ Server/server.js
@@ -11,29 +11,48 @@ server.use(cors());
 
 let productsJson = [
     {
-      image: "../assets/board_birds.jpg",
-      title: "Women's Saloman Wonder Snowboard",
-      description: "One of Salomon's most versatile boards, the Wonder is designed for maximum performance and playfulness on all terrains, in all conditions. An advanced directional twin shape is built for switch riding, with lengthened contact points for soft snow performance that won't affect handling on groomers.",
-      price: "$629.99",
-      stock: "10 available"
+        id: 1,
+        image: "../assets/saloman1.jpg",
+        image2: "../assets/saloman2.jpg",
+        image3: "../assets/saloman3.jpg",
+        image4: "../assets/saloman4.jpg",
+        image5: "../assets/saloman5.jpg",
+        title: "Salomon Wonder Snowboard - Women's 2022",
+        description: "One of Salomon's most versatile boards, the Wonder is designed for maximum performance and playfulness on all terrains, in all conditions. An advanced directional twin shape is built for switch riding, with lengthened contact points for soft snow performance that won't affect handling on groomers.",
+        price: 479.99,
+        stock: 10,
     },
     {
-      image: "../assets/board_burton.jpg",
-      title: "Women’s Burton Feelgood Camber Snowboard",
-      description: "Backed by Kelly Clark, the women’s Burton Feelgood Snowboard has been the defining force in women’s snowboarding for two decades. The Feelgood boasts a unique shape, matched with positively powerful pop for Ferrari-like handing.",
-      price: "$739.99",
-      stock: "5 available"
+        id: 2,
+        image: "../assets/burton1.jpg",
+        image2: "../assets/burton2.jpg",
+        image3: "../assets/burton3.jpg",
+        image4: "../assets/burton4.jpg",
+        image5: "../assets/burton5.jpg",
+        title: "Women’s Burton Feelgood Camber Snowboard",
+        description: "Backed by Kelly Clark, the women’s Burton Feelgood Snowboard has been the defining force in women’s snowboarding for two decades. The Feelgood boasts a unique shape, matched with positively powerful pop for Ferrari-like handing.",
+        price: 629.99,
+        stock: 5,
     },
     {
-      image: "../assets/board_teal.jpg",
-      title: "Women’s Saloman Rumble Fish Snowboard",
-      description: "A women’s specific quiver killer blending our most popular powder and freestyle creations. Rock Out Camber delivers versatility in all conditions freestyle, while the directional twin shape provides added buoyancy in deep snow. ABC Wrapper design substitutes traditional materials for bamboo, fusing sustainability and innovation for a lively board.",
-      price: "$549.99",
-      stock: "3 available"
+        id: 3,
+        image: "../assets/assassin1.jpg",
+        image2: "../assets/assassin2.jpg",
+        image3: "../assets/assassin3.jpg",
+        image4: "../assets/assassin4.jpg",
+        image5: "../assets/assassin5.jpg",
+        title: "Women’s Salomon Lotus Snowboard",
+        description: "Like a friend who waits for you to get up and clean off your goggles on a powder day, the Salomon Lotus Snowboard has that rare combination of trusty performance and easygoing likeability you need to take your riding to the next level. A directional twin with a 10mm setback for stability at speed and float in the deep stuff, the Lotus features a classic single radius sidecut and a forgiving Flat Out Camber profile.",
+        price: 549.99,
+        stock: 3,
     }
-  
 ]
 
-server.get('/', (req, res) => {
+server.get('/womens', (req, res) => {
     res.send(productsJson);
+})
+
+server.get('/womens/:productid', (req, res) =>{
+    let id_from_client = req.params.productid;
+    res.json(productsJson.find(x => x.id == id_from_client))
 })
